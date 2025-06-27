89a619efd9264635473b456a2746f28c3e2dbc4f
William <william1504@gmail.com>
2025-06-27 15:44:11 -0300

    random spot

--- src/enemy.rs
+++ src/enemy.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use rand::Rng;
 
 use crate::{
     game_state::GameState,
@@ -19,6 +20,18 @@ pub enum WeakSpotLocation {
 }
 
 impl WeakSpotLocation {
+    pub fn random() -> Self {
+        let mut rng = rand::rng();
+        let dir = rng.random_range(0..4);
+
+        match dir {
+            0 => WeakSpotLocation::North,
+            1 => WeakSpotLocation::South,
+            2 => WeakSpotLocation::West,
+            _ => WeakSpotLocation::East,
+        }
+    }
+
     pub fn to_dir(&self) -> Vec3 {
         match self {
             WeakSpotLocation::North => Vec3::Y,
@@ -38,7 +51,7 @@ pub struct WeakSpot {
 impl WeakSpot {
     pub fn new(size: f32) -> Self {
         Self {
-            location: WeakSpotLocation::default(),
+            location: WeakSpotLocation::random(),
             size,
         }
     }
@@ -53,6 +66,64 @@ impl Plugin for EnemyPlugin {
 }
 
 fn spawn_enemy(mut commands: Commands) {
+    let weak_spot = WeakSpot::new(16.);
+    let weak_spot_pos = weak_spot.location.to_dir() * 16.;
+
+    commands
+        .spawn((
+            Enemy,
+            Velocity::default(),
+            Transform::from_translation(Vec3::new(0., 200., 0.)),
+            Radius(16.),
+            Health::new(1),
+            weak_spot,
+            Airborne,
+            Sprite {
+                color: Color::srgb_u8(200, 10, 10),
+                custom_size: Some(Vec2::splat(32.)),
+                ..default()
+            },
+        ))
+        .with_children(|parent| {
+            parent.spawn((
+                Transform::from_translation(Vec3::new(weak_spot_pos.x, weak_spot_pos.y, 1.)),
+                Sprite {
+                    color: Color::srgb_u8(200, 200, 10),
+                    custom_size: Some(Vec2::splat(12.)),
+                    ..default()
+                },
+            ));
+        });
+
+    let weak_spot = WeakSpot::new(16.);
+    let weak_spot_pos = weak_spot.location.to_dir() * 16.;
+
+    commands
+        .spawn((
+            Enemy,
+            Velocity::default(),
+            Transform::from_translation(Vec3::new(0., 200., 0.)),
+            Radius(16.),
+            Health::new(1),
+            weak_spot,
+            Airborne,
+            Sprite {
+                color: Color::srgb_u8(200, 10, 10),
+                custom_size: Some(Vec2::splat(32.)),
+                ..default()
+            },
+        ))
+        .with_children(|parent| {
+            parent.spawn((
+                Transform::from_translation(Vec3::new(weak_spot_pos.x, weak_spot_pos.y, 1.)),
+                Sprite {
+                    color: Color::srgb_u8(200, 200, 10),
+                    custom_size: Some(Vec2::splat(12.)),
+                    ..default()
+                },
+            ));
+        });
+
     let weak_spot = WeakSpot::new(16.);
     let weak_spot_pos = weak_spot.location.to_dir() * 16.;
 
