3bd6c3a142786be82cea298d13620fc157fc21dd
William <william1504@gmail.com>
2025-06-29 13:26:14 -0300

    add pages ci

--- src/args.rs
+++ src/args.rs
@@ -4,6 +4,6 @@ use clap::Parser;
 #[derive(Parser, Resource, Debug, Clone)]
 pub struct Args {
     /// runs the game in debug mode
-    #[clap(long, alias="dbg")]
+    #[clap(long, alias = "dbg")]
     pub debug: bool,
 }
